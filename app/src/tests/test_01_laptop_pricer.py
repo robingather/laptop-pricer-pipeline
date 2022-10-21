bfcb8af70e2dd23dd8898690cadcc178c6e80057
Robin Gather <rgather11@gmail.com>
2022-10-21 11:53:08 +0200

    t

--- app/src/tests/test_01_laptop_pricer.py
+++ app/src/tests/test_01_laptop_pricer.py
@@ -7,7 +7,7 @@ import laptop_pricer
 
 class TestDiabetesPredictor:
     def test_predict_single_record(self):
-        with open('prediction_request.json') as json_file:
+        with open('tests/prediction_request.json') as json_file:
             data = pd.read_json(json_file)
         dp = laptop_pricer.LaptopPricePredictor()
         status = dp.predict_single_record(data)
