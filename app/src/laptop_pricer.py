4b9e322a3257b9bcb407413e9628693ad0c93516
Robin Gather <rgather11@gmail.com>
2022-10-22 12:12:00 +0200

    request.json support

--- app/src/laptop_pricer.py
+++ app/src/laptop_pricer.py
@@ -11,9 +11,13 @@ class LaptopPricePredictor:
     def __init__(self):
         self.model = None
 
+    def download_test_model(self):
+        self.model = pickle.load(open('tests/laptop_pricer_model.pkl', 'rb'))
+        print("downloaded test model")
+        return jsonify({'message': " the model was downloaded"}), 200
+
     # download the model
     def download_model(self):
-        print("thank the lord u made it here")
         project_id = os.environ.get('PROJECT_ID', 'Specified environment variable is not set.')
         model_repo = os.environ.get('MODEL_REPO', 'Specified environment variable is not set.')
         model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
@@ -27,15 +31,20 @@ class LaptopPricePredictor:
 
     def predict_single_record(self, prediction_input):
         print(prediction_input)
-        #if self.model is None:
-        #    self.download_model()
+        if self.model is None:
+           self.download_test_model()
         #print(json.dumps(prediction_input))
         #df = pd.read_json(json.dumps(prediction_input), orient='records')
         print("hEY", file=sys.stderr)
         print(prediction_input.values,  file=sys.stderr)
         print(prediction_input.values[0],  file=sys.stderr)
+        print("gonna predithtttt")
         y_pred = self.model.predict(prediction_input.values)
-        print(y_pred[0])
-        print(type(y_pred[0]))
+        # print(y_pred[0])
+        # print(y_pred[0][0])
+        # print(y_pred[0,0])
+        # print(float(y_pred[0]))
+        # print(float(y_pred[0][0]))
+        # print(type(y_pred[0]))
         # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
-        return jsonify({'result': float(y_pred[0])}), 200
+        return jsonify({'result': str(y_pred[0][0])}), 200
