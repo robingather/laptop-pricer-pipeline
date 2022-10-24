7c6f785c4ef46478e13f4849b3e687450449c2d5
Robin Gather <rgather11@gmail.com>
2022-10-24 16:52:15 +0200

    badtest

--- app/src/tests/test_01_laptop_pricer.py
+++ app/src/tests/test_01_laptop_pricer.py
@@ -1,19 +1,25 @@
 # content of test_sysexit.py
 import pandas as pd
+import app
 
 # content of test_class.py
 import laptop_pricer
+from flaskr import flaskr
 
 
 class TestLaptopPricer:
     def test_predict_single_record(self):
+
+        flaskr.app.config['TESTING'] = True
         with open('testResources/prediction_request.json') as json_file:
             data = pd.read_json(json_file, orient='records')
         dp = laptop_pricer.LaptopPricePredictor()
         print(data)
         print(data.values)
         print("testing")
-        y_pred = dp.predict_single_record(data)
+        with flaskr.app.test_client() as client:
+            with flaskr.app.app_context():
+                y_pred = dp.predict_single_record(data)
         print("prediction=",y_pred)
 
         result = y_pred[0]
@@ -29,3 +35,4 @@ class TestLaptopPricer:
         assert y_pred[0] > 0 # you shouldn't be paid to buy a laptop
         print("assertion 3")
         assert str(y_pred[0]).isnumeric() # should be a number
+        flaskr.app.config['TESTING'] = False
