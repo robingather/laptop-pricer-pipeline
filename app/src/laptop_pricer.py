8b3b056814040c1adfe2f92e9be297a1d5d5b73f
Robin Gather <rgather11@gmail.com>
2022-10-25 18:00:59 +0200

    not test model

--- app/src/laptop_pricer.py
+++ app/src/laptop_pricer.py
@@ -31,7 +31,7 @@ class LaptopPricePredictor:
     def predict_single_record(self, prediction_input, asjson = True):
         print(prediction_input)
         if self.model is None:
-           self.download_test_model()
+           self.download_model()
         #print(json.dumps(prediction_input))
         #df = pd.read_json(json.dumps(prediction_input), orient='records')
         print("hEY", file=sys.stderr)
